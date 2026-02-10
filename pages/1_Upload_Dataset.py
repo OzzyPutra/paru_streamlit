7c9cc68a4b71d2f154d7264d5959de0f0fe96205
OzzyPutra <111202013031@mhs.dinus.ac.id>
2026-02-10 17:22:45 +0700

    Update 1_Upload_Dataset.py

--- pages/1_Upload_Dataset.py
+++ pages/1_Upload_Dataset.py
@@ -71,5 +71,5 @@ if os.path.exists(dataset_paruparu):
                     #label="💾 Download Model",
                     #data=f,
                     #file_name="naive_bayes_model.pkl"
-                )
+
 
